1f759cade66cc860633e1e56e9fdf8e8270d460a
Dai Thang <23521422@gm.uit.edu.vn>
2026-03-14 01:11:57 +0700

    feat(auth): seed keycloak realm/client/roles/users + gateway jwt auth (jwks) + rbac guard

--- services/gateway/src/main.ts
+++ services/gateway/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,11 +11,13 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('DocVault Gateway')
     .setVersion('0.1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  const port = Number(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 bootstrap();
