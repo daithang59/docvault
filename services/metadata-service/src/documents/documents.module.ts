519241dc2c770f780870ed43a005c523ea06a72f
Dai Thang <23521422@gm.uit.edu.vn>
2026-03-14 22:26:51 +0700

    feat(storage): upload documents to minio and persist object metadata in postgres

--- services/metadata-service/src/documents/documents.module.ts
+++ services/metadata-service/src/documents/documents.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
+  imports: [StorageModule],
   controllers: [DocumentsController],
   providers: [DocumentsService],
 })
 export class DocumentsModule {}
+
