6ee1387c429e938960e5567720e3855e8b2687fb
duyimew <23520380@gm.uit.edu.vn>
2026-04-27 20:02:33 +0700

    fix(ci): fixed some vulnerabilities scanned by trivy fs, checkov

--- vars/sonarSast.groovy
+++ vars/sonarSast.groovy
@@ -2,18 +2,24 @@ def call(cfg) {
     echo '>>> Running SAST Scan (SonarQube)...'
 
     withSonarQubeEnv(cfg.sonarQubeInstallation) {
-        sh """
+        withEnv([
+            "SONAR_SCANNER_IMAGE=${cfg.sonarScannerImage}",
+            "SONAR_PROJECT_KEY=${cfg.sonarProjectKey}",
+            "SONAR_HOST_FALLBACK=${cfg.sonarHostUrl}"
+        ]) {
+            sh '''
             set -eu
-            SONAR_HOST=\"\${SONAR_HOST_URL:-${cfg.sonarHostUrl}}\"
+            SONAR_HOST="${SONAR_HOST_URL:-$SONAR_HOST_FALLBACK}"
             docker run --rm \\
                 --network host \\
-                -v ${env.WORKSPACE}:/usr/src \\
+                -v "$WORKSPACE:/usr/src" \\
                 -w /usr/src \\
-                -e SONAR_TOKEN=\"$SONAR_AUTH_TOKEN\" \\
-                ${cfg.sonarScannerImage} \\
-                -Dsonar.projectKey=${cfg.sonarProjectKey} \\
+                -e SONAR_TOKEN="$SONAR_AUTH_TOKEN" \\
+                "$SONAR_SCANNER_IMAGE" \\
+                -Dsonar.projectKey="$SONAR_PROJECT_KEY" \\
                 -Dsonar.sources=. \\
                 -Dsonar.host.url=\"$SONAR_HOST\"
-        """
+            '''
+        }
     }
 }
