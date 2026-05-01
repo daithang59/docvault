380234ec0b2646776c548aba29c832fbb11caf2c
Dai Thang <23521422@gm.uit.edu.vn>
2026-05-01 23:16:11 +0700

    ci: update dependency check and iac scan scripts for improved functionality

--- vars/dependencyCheck.groovy
+++ vars/dependencyCheck.groovy
@@ -1,23 +1,26 @@
 def call() {
     echo '>>> Running SCA Scan...'
-    sh 'mkdir -p dependency-check-report'
+    sh '''
+        mkdir -p dependency-check-report
+        mkdir -p /var/jenkins_home/dependency-check-data
+    '''
+
     catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
         sh """
             docker run --rm \\
                 -v ${env.WORKSPACE}:/src \\
                 -v ${env.WORKSPACE}/dependency-check-report:/report \\
-                -v /home/abby/jenkins_workspace/dependency-check-data:/usr/share/dependency-check/data \\
+                -v /var/jenkins_home/dependency-check-data:/usr/share/dependency-check/data \\
                 owasp/dependency-check:latest \\
-                --project \"DocVault\" \\
+                --project "DocVault" \\
                 --scan /src \\
-                --exclude \"**/node_modules/**\" \\
-                --exclude \"**/.pnpm-store/**\" \\
-                --format \"HTML\" \\
-                --format \"JSON\" \\
+                --exclude "**/node_modules/**" \\
+                --exclude "**/.pnpm-store/**" \\
+                --format "HTML" \\
+                --format "JSON" \\
                 --out /report \\
                 --failOnCVSS 7 \\
-                --disableKnownExploited \\
-                --noupdate
+                --disableKnownExploited
         """
     }
 }
